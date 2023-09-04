385cc7c007a3c0f1cb5bc969e9e4ea2510b86761
unknown <alejandro7899871776@gmail.com>
2023-09-04 16:11:29 -0500

    adding thoughs in the UX, Also adding math chains duck duck

--- test2.py
+++ test2.py
@@ -13,12 +13,14 @@ from langchain.llms import OpenAI
 from langchain.tools import DuckDuckGoSearchRun
 
 from langchain.utilities import PythonREPL
-
+os.environ["LANGCHAIN_TRACING"] = "true"
 #%% Api keys
 openai_api_key = ""
 from langchain.tools import DuckDuckGoSearchRun
 from langchain.agents import load_tools, initialize_agent, AgentType,Tool
-
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain import LLMMathChain
 
 # SerpAPI_api_key=
 
@@ -28,38 +30,50 @@ search = DuckDuckGoSearchRun()
 # Python repl
 python_repl = PythonREPL()
 
-# tools = [  Tool(
-#     name='DuckDuckGo Search',
-#     func= search.run,
-#     description="Useful for when you need to do a search on the internet to find information that another tool can't find. be specific with your input."
-# )]
-    
+llm = ChatOpenAI(temperature=0.5,openai_api_key=openai_api_key,model_name="gpt-3.5-turbo-16k-0613", streaming=True )
+llm_math_chain = LLMMathChain.from_llm(llm=llm, verbose=True)
 
-# python_repl = Tool(
-#     name = "python repl",
-#     func=python_repl.run,
-#     description="useful for when you need to use python to answer a question. You should input python code or math questions"
-# )
-    
 
-tools = [
+tools = [  Tool(
+    name='DuckDuckGo Search',
+    func= search.run,
+    description="useful for when you need to answer questions about current events"
+),
     Tool(
-        name="Intermediate Answer",
-        func=search.run,
-        description="useful for searching the web",
-    )
-]
+        name="Calculator",
+        func=llm_math_chain.run,
+        description="useful for when you need to answer questions about math"
+    )]
+    
+
+
+   
+
+
+#%% streaming 
+
+            
+#%%             
+
+
+# tools = [
+#     Tool(
+#         name="Intermediate Answer",
+#         func=search.run,
+#         description="useful for searching the web",
+#     )
+# ]
     
 # tools.append(python_repl)
 
 
-llm = OpenAI(temperature=0.5,openai_api_key=openai_api_key,model_name="gpt-4" )
 
 
 # tools = load_tools(["serpapi"], llm=llm)
-self_ask_with_search = initialize_agent(
+zero_shot_agent = initialize_agent(
+    agent="zero-shot-react-description",
     # agent="zero-shot-react-description",
-    agent=AgentType.SELF_ASK_WITH_SEARCH,
+    # agent=AgentType.SELF_ASK_WITH_SEARCH,
     tools=tools,
     llm=llm,
     verbose=True,
@@ -70,23 +84,43 @@ self_ask_with_search = initialize_agent(
 #%% app
 
 
+content = """Complete the objective as best you can. You have access to the following tools:
+
+{tools}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+These were previous tasks you completed:
+
+
+
+Begin!"""
+
+from langchain.callbacks import StreamlitCallbackHandler
 
 def init():
-
     st.set_page_config(
         page_title="MinckatGPT",
-
     )
 
 def main():
     init()
+    
+    # Create StreamlitCallbackHandler instance
+    st_callback = StreamlitCallbackHandler(st.container())
 
-    # chat = ChatOpenAI(temperature=0.7, openai_api_key=openai_api_key)
-
-    # initialize message history
     if "messages" not in st.session_state:
         st.session_state.messages = [
-            SystemMessage(content="You are a helpful assistant that provides succint answers using clear language, you have some tool to search the web and ask yourself question whe providing answers clean them avoiding thirdparty information in the web just the content")
+            SystemMessage(content=content)
         ]
 
     st.header("MinckaGPT")
@@ -95,23 +129,21 @@ def main():
     with st.container():
         user_input = st.text_input("Your message: ", key="user_input")
 
-        # handle user input
         if user_input:
             st.session_state.messages.append(HumanMessage(content=user_input))
             with st.spinner("Thinking..."):
-                response =self_ask_with_search.run(st.session_state.messages)
+                response = zero_shot_agent.run(st.session_state.messages,callbacks=[st_callback])
             st.session_state.messages.append(
                 AIMessage(content=response))
+            
+            # If you want to use the StreamlitCallbackHandler, you may need to link it here.
 
-    # display message history
     messages = st.session_state.get('messages', [])
     for i, msg in enumerate(messages[1:]):
         if i % 2 == 0:
             message(msg.content, is_user=True, key=str(i) + '_user', avatar_style='no-avatar')
         else:
             message(msg.content, is_user=False, key=str(i) + '_ai', avatar_style='no-avatar')
-
-
 
 if __name__ == '__main__':
     main()
